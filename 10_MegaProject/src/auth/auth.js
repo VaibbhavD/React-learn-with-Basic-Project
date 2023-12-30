061f3e0ba4a56d4b816ded2aaeb0374fcdae2b76
VaibhavDhamanage <vaibhavdhamanage>
2023-12-30 09:39:59 +0530

    build Authenticate sign up , login and log out

---
+++ 10_MegaProject/src/auth/auth.js
@@ -0,0 +1,62 @@
+import conf from "../conf/conf";
+import {Client,Account,ID} from "appwrite"
+
+
+export class AuthService{
+    client=new Client();
+    account;
+
+    constructor(){
+        this.client
+        .setEndpoint(appwriteUrl)  
+        .setProject(appwriteProjectId);      
+        
+        this.account=new Account(this.client)
+    }
+
+    async createAccount(email,password,name){
+        try {
+            const useraccount= await this.account.create(ID.unique(),email,password,name)
+             if (useraccount) {
+                return this.login(email,password)
+             }
+             else{
+                return useraccount;
+             }
+        }
+         catch (error) {
+            throw error;
+        }
+    }
+
+    async login(email,password){
+        try {
+            return await this.account.createEmailSession(email,password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentuser(){
+        try {
+            return await this.account.get()
+        }
+         catch (error) {
+            throw error;
+        }
+        return null;
+    }
+
+    async logout(){
+        try {
+            return await this.account.deleteSessions();
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+
+const authService=new AuthService()
+
+export default authService
