2a5f1543bab540e4f63093740f09543daf5d9fe0
VaibhavDhamanage <vaibhavdhamanage>
2023-12-30 16:39:33 +0530

    Add Header And Footer File

--- 10_MegaProject/src/App.jsx
+++ 10_MegaProject/src/App.jsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from 'react'
 import './App.css'
-import conf from './conf/conf'
+import {Header,Footer} from './Components/index'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import {login,logout} from './store/authSlice'
 
 
 function App() {
-  console.log(conf.appwriteBucketId)
-  return (
-   <div>
-   </div>
+  const[loading,setloading]=useState(true);
+  const dispatch=useDispatch();
+ 
+  useEffect(()=>{
+    authService.getCurrentuser()
+    .then((userdata)=>{
+           if(userdata)   
+           {
+            dispatch(login(userdata))
+           } 
+           else{
+            dispatch(logout())
+           }})
+    .finally(()=>setloading(false))
+    },[])
+  
+  return !loading?(
+    <div className="w-full h-full bg-black">hi Vaibhav</div>
+  ):(
+    <div className="w-full h-full bg-black">Please Login First</div>
   )
 }
 
